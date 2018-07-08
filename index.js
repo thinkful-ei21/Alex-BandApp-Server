009c746acb75152a2bc32ab77e773f55eec58f71
Alex Widner <manny1806@gmail.com>
2018-07-07 19:46:33 -0700

    auth working

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const { router: usersRouter } = require('./routes/users');
-const { router: authRouter, localStrategy, jwtStrategy } = require('./auth/strategies');
+const { jwtStrategy } = require('./auth/strategies');
+const { localStrategy } = require('./auth/strategies');
 
+const authRouter = require('./routes/auth')
 const postRouter = require('./routes/posts');
 const eventRouter = require('./routes/events');
 
@@ -30,13 +32,27 @@ app.use(
   })
 );
 
-passport.use(localStrategy);
-passport.use(jwtStrategy);
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   if (req.method === 'OPTIONS') {
+//     return res.send(204);
+//   }
+//   next();
+// });
+
+passport.use( localStrategy)
+passport.use( jwtStrategy);
+
+//const jwtAuth = passport.authenticate('jwt', { session: false });
 
 app.use(express.json());
 
 app.use('/api/posts', postRouter);
 app.use('/api/events', eventRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter.router);
 
 function runServer(port = PORT) {
   const server = app
