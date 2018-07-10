dfb89abd70cff18339bf6ec1bea279224c25f2b1
manny1806 <manny1806@gmail.com>
2018-07-10 01:42:14 -0700

    m

--- routes/bands.js
+++ routes/bands.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const Band = require('../models/bands');
+const User = require('../models/users')
 
 const router = express.Router();
 
