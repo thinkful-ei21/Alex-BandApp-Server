22bfad89ca71e68a47b6c1752e86a2be57739247
manny1806 <manny1806@gmail.com>
2018-07-10 03:19:12 -0700

    m

--- routes/users.js
+++ routes/users.js
@@ -136,6 +136,10 @@ router.post('/', jsonParser, (req, res) => {
     });
 });
 
+router.post('/checkExist', (req, res) =>{
+  return User.find({username: req.body.username}).count()
+    .then((count) => res.json(count))
+})
 // Never expose all your users like below in a prod application
 // we're just doing this so we have a quick way to see
 // if we're creating users. keep in mind, you can also
