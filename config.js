c8523fdb35c88fa44ea68fe845de7f1773cfce3d
manny1806 <manny1806@gmail.com>
2018-07-13 19:28:38 -0700

    m

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/art-stars',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/art-stars',
+//   DATABASE_URL:
+//         process.env.DATABASE_URL || 'mongodb://localhost/art-stars',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
